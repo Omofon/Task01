fa3176f4de14d109c534a92c02927342d885c890
Omofon <114416186+Omofon@users.noreply.github.com>
2022-10-22 19:38:19 +0000

    i declared and assigned a value to stage

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ console.log(`Hello ${userName} 😊`);
 console.log('');
 
 // introduction to the game
-console.log(`Welcome to my number guessing game ${userName}`)
-console.log('You are to guess a number within the range.')
+console.log(`Welcome to my number guessing game ${userName}.`)
+console.log('You are to guess a number within a range.')
 console.log("If you guess right, you will be awarded a point. If not you will not gain no points.");
 console.log("LET'S GO!!!");
 console.log('')
@@ -20,13 +20,15 @@ console.log('')
 let stage = parseInt(prompt("How many stages are you going? "));
 let rep = stage + 1;
 let min = 1, max = 1, point = 0;
+console.log();
 
 // looping for max
 while (max < rep) {
   max++
   let inputNum = parseInt(prompt(`Guess between ${min} and ${max}`));
   console.log('');
-  // function rand to generate random number
+  
+// function rand to generate random number
   const rand = () => {
 
     //console.log(`Start value= ${min}; Range Value= ${max}`);
@@ -39,14 +41,13 @@ while (max < rep) {
       console.log('')
 
     };
-
     if (inputNum == result) {
       point++
-      console.log(`You're correct! You have ${point} out of ${rep} points`);
+      console.log(`You're correct! You have ${point} out of ${stage} points`);
       console.log('')
     };
     if (inputNum !== result) {
-      console.log(`You're wrong! You have ${point} out of ${rep} points`);
+      console.log(`You're wrong! You have ${point} out of ${stage} points`);
       console.log(`The correct guess is ${result}`);
       console.log('')
     };
