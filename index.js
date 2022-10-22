d2a03fbe184d042a771ea1ce10b70a9cfd2644cb
Omofon <114416186+Omofon@users.noreply.github.com>
2022-10-22 17:20:01 +0000

    Final edit of the game

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
-// declaring and assigning range
-/*
-let range = function(min, max) {
-  let min = 1;
-  let max = 2;
 
-  return range;
-};
-*/
+// header
 console.log("A NUMBER GUESSING GAME");
 console.log('');
-/*
-let start = 1;
-let range = 2;
-*/
-//generate a number from given range
 
-function randomNumber(start, range, score){
+// username prompt
+var userName = prompt("Enter your name: ");
+console.log(`Hello ${userName} 😊`);
 
-// prompt for user name
-  const userName = prompt("Enter your name: ");
-  console.log(`Hello ${userName}`);
+// introduction to the game
+console.log(`Welcome to my number guessing game ${userName}`)
+console.log('You are to guess a number within the range.')
+console.log("If you guess right, you will be awarded a point. If not you will not gain no points.");
+console.log("LET'S GO!!!");
+
+// declaring and assigning values
+console.log('')
+let rep = parseInt(prompt("How many stages are you going? ")) + 1;
+let min = 1, max = 1, point = 0;
+
+// looping for max
+while (max < rep) {
+
+  max++
+  let inputNum = parseInt(prompt(`Guess between ${min} and ${max}`));
   console.log('');
-  console.log(`Start value: ${start};/nRange Value: ${range}`);
+  // function rand to generate random number
+  const rand = () => {
+    //console.log(`Start value= ${min}; Range Value= ${max}`);
+    var result = Math.floor(Math.random() * (max - min + 1)) + min;
+    //console.log(result)
 
-  score = 0;
-  start = 1;
-  range = 2;
-  let getRandom = Math.floor(Math.random() * (range - start + 1)) + start;
-//  return getRandom;
+    if (inputNum > max || inputNum < min) {
+      console.log("You're input is out of range");
+      inputNum = parseInt(prompt(`Guess between ${min} and ${max}`));
+    };
 
-  let inputNum = parseInt(prompt(`Guess between ${start} and ${range}`));
-  console.log('');
-
-  if (inputNum > range || inputNum < start) {
-  console.log("You're out of range"); 
-    return inputNum;
-
-  if (inputNum == getRandom) {
-  score++
-  console.log(`You're correct! /n Your score is ${score}`);
+    if (inputNum == result) {
+      point++
+      console.log(`You're correct! You have ${point} out of ${rep} points`);
+    };
+    if (inputNum !== result) {
+      console.log(`You're wrong! You have ${point} out of ${rep} points`);
+      console.log(`The correct guess is ${result}`);
+    };
   };
-  if (inputNum !== getRandom) {
-  console.log(`You're wrong! /n Your score is ${score}`);
-   randomNumber();
+  rand();
+  console.log();
+
+  if (max === rep) {
+    console.log('THE END.')
+    break;
   };
 };
+
+
+
